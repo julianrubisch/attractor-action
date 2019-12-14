40a245dbf842c1f25221bbed43d6e1ce24d1fb7e
Julian Rubisch <julian@julianrubisch.at>
2019-12-14 21:16:40 +0100

    Update index.rb

--- index.rb
+++ index.rb
@@ -2,5 +2,4 @@ command = "attractor report --ci"
 command += " --file_prefix #{ENV['INPUT_FILE_PREFIX']}" unless ENV['INPUT_FILE_PREFIX']&.empty?
 command += " -t js" unless ENV['INPUT_JAVASCRIPT']&.empty?
 
-system("git log")
 system("#{command}")
