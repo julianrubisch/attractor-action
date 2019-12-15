894c84c51f6be8147ada4649be4a49860d09ec91
Julian Rubisch <julian@julianrubisch.at>
2019-12-15 08:01:19 +0100

    Update index.rb

--- index.rb
+++ index.rb
@@ -1,6 +1,6 @@
 command = "attractor report --ci"
 command += " --file_prefix #{ENV['INPUT_FILE_PREFIX']}" unless ENV['INPUT_FILE_PREFIX']&.empty?
-command += " -t js" unless ENV['INPUT_JAVASCRIPT']&.empty?
+command += " -t js" unless ENV['INPUT_JAVASCRIPT']
 
 puts command
 
